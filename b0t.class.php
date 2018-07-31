a4c637aaa2f906cc30c678f40d66e81a9a930ca9
zararli <kursat.vurusan@metu.edu.tr>
2018-07-31 03:08:28 +0300

    new version of husnab0t

---
+++ b0t.class.php
@@ -0,0 +1,265 @@
+<?php
+require_once("settings.php");
+class husna extends settings
+{
+    private $senderUsername;
+    private $senderFirstName;
+    private $senderLastName;
+    private $message;
+    private $firstWord;
+    private $otherWords; 
+    private $messageId;
+    private $chatId;
+    private $userEnter;
+    private $userExit;
+    private $groupOrPrivate;
+    private $reqUrl;
+    private $commands = array();
+    private $whoamI;
+    public function __construct($data)
+    {
+        $this->senderUsername = $data["message"]["from"]["username"];
+        $this->senderFirstName = $data["message"]["from"]["first_name"];
+        $this->senderLastName = $data["message"]["from"]["last_name"];
+        $this->message = $data["message"]["text"];
+        $this->firstWord = explode(" ",$this->message,2)[0];
+        $this->otherWords = explode(" ",$this->message,2)[1];
+        $this->chatId = $data["message"]["chat"]["id"];
+        $this->userEnter = ($data["message"]["new_chat_member"]["username"]) ? $data["message"]["new_chat_member"]["username"] : 0;
+        $this->userExit = ($data["message"]["left_chat_participant"]["username"]) ? $data["message"]["left_chat_participant"]["username"] : 0;
+        $this->groupOrPrivate = ($data["message"]["chat"]["type"] == "private") ? 0 : 1; // 0 = private message, 1 = group message
+        $this->reqUrl = "https://api.telegram.org/bot".$this->getBotToken() . "/";
+        $this->whoamI = 
+        "ben hüsna b0t\n
+        bilgiad yazarsan sana harika bilgiler getiririm
+        \n####\n
+        allambilgiat {söz öbeği} formatında şeyler söylersen de arar tarar senin için o şeyi bulurum
+        \n####\n
+        getirhoca {id}|{ad-soyad} yazarsan senin için okulda o öğrenciyi ararım
+        \n####\n
+        mizahyab yazarsan senin için birbirinden eĞLenCeLi fıkralarımdan birisini anlatırım.
+        \n####\n
+        fotoad yazarsan senin için internetin derinliklerinden elde ettiğim görsellerimden birisini paylaşırım
+        \n####\n
+        henüz yeni sayılırım mazur gör hoja";
+
+
+    }
+
+    public function proccess(){
+        if($this->getUserEnter() !== 0)
+        {
+            $this->sayHi();
+            return;
+        }
+        else if($this->getUserExit() !== 0)
+        {
+            $this->sayBye();
+            return;
+        }
+
+        foreach ($this->getCommands() as $key => $value) {
+            if (strpos($this->message, $key) !== false) {
+                $value();
+            }
+        }
+        die();
+    }
+
+    /*
+        sendMessage function takes 
+        $message : text of the message to be sent
+        $reply_to_message (optional, default 0) : sends messages as reply if <> 0
+    */
+
+
+    public function sendMessage($message,$reply_to_message = 0) {
+        $reply = ($reply_to_message != 0) ? "&reply_to_message_id=".$this->getMessageId(): "";
+        $url = "sendMessage?chat_id=".$this->getChatId()."&text=".urlencode($message).$reply;
+        $this->requEst($url);
+        return;
+    }
+
+    /*
+        sendPhoto function takes 
+        $photoUrl : image file link, any common extension(gif,jpeg,png etc.)
+        $caption (optional, default 0) : image message caption, 0-200 characters
+        $reply_to_message (optional, default 0) : sends messages as reply if <> 0
+
+    */
+
+
+    public function sendPhoto ($photoUrl, $caption = "",$reply_to_message = 0) {
+        $reply = ($reply_to_message != 0) ? "&reply_to_message_id=".$this->getMessageId(): "";
+        $caption = ($caption !== "") ? "&caption=" . urlencode(substr($caption,0,200)) : "";
+        $url = "sendPhoto?chat_id=".$this->getChatId()."&photo=".urlencode($photoUrl). $caption . $reply;
+        $this->requEst($url);    
+    }
+    /*
+        sendVoiceMessage function takes 
+        $voice : sound file link, only ogg extension is allowed
+        $caption (optional) : voice message caption, 0-200 characters
+        $reply_to_message (optional, default 0): sends messages as reply if <> 0
+
+    */
+    public function sendVoiceMessage ($voice, $caption="",$reply_to_message = 0) {
+
+        $reply = ($reply_to_message != 0) ? "&reply_to_message_id=". $this->getMessageId()  : "";
+        $url = "sendVoice?chat_id=".$this->getChatId()."&voice=".urlencode($voice)."&caption=".urlencode(substr($caption,0,200)).$reply;
+        $this->requEst($url);
+    }
+
+
+    private function sayHi(){
+            if($this->getUserEnter() == "HusnaBot" ){
+                $this->sendVoiceMessage("https://kursat.blog/b0t/audio/selam.ogg","hey bitchezzz");
+                $this->sendMessage($this->getWhoamI());
+            }else{
+                $this->sendVoiceMessage("https://kursat.blog/b0t/audio/selam.ogg","@".$this->getUserEnter());
+            }
+    }
+    private function sayBye(){
+        $this->sendVoiceMessage("https://kursat.blog/b0t/audio/seriuzgunad.ogg","@".$this->getUserExit(). " gitti. artık bir eksiğiz...");
+    }
+    private function requEst($url){
+
+        $output =  $this->getReqUrl() . $url;
+ 
+        $ac = fopen("reqs.txt","a+");
+        fwrite($ac,$output."\n");
+        
+        fclose($ac);
+
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->getReqUrl() . $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+		curl_exec($ch);
+		curl_close($ch);
+    }
+
+    /**
+     * Get the value of senderUsername
+     */
+    public function getSenderUsername()
+    {
+        return $this->senderUsername;
+    }
+
+    /**
+     * Get the value of senderFirstName
+     */
+    public function getSenderFirstName()
+    {
+        return $this->senderFirstName;
+    }
+
+    /**
+     * Get the value of senderLastName
+     */
+    public function getSenderLastName()
+    {
+        return $this->senderLastName;
+    }
+
+    /**
+     * Get the value of message
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Get the value of chatId
+     */
+    public function getChatId()
+    {
+        return $this->chatId;
+    }
+
+    /**
+     * Get the value of userEnter
+     */
+    public function getUserEnter()
+    {
+        return $this->userEnter;
+    }
+
+    /**
+     * Get the value of userExit
+     */
+    public function getUserExit()
+    {
+        return $this->userExit;
+    }
+
+    /**
+     * Get the value of groupOrPrivate
+     */
+    public function getGroupOrPrivate()
+    {
+        return $this->groupOrPrivate;
+    }
+
+    /**
+     * Get the value of messageId
+     */ 
+    public function getMessageId()
+    {
+        return $this->messageId;
+    }
+
+    /**
+     * Set the value of commands
+     *
+     * @return  self
+     */ 
+    public function addCommand($command,$function_name)
+    {
+        $this->commands += array($command=>$function_name);
+        return $this;
+    }
+
+    /**
+     * Get the value of reqUrl
+     */ 
+    public function getReqUrl()
+    {
+        return $this->reqUrl;
+    }
+
+    /**
+     * Get the value of whoamI
+     */ 
+    public function getWhoamI()
+    {
+        return $this->whoamI;
+    }
+
+    /**
+     * Get the value of commands
+     */ 
+    public function getCommands()
+    {
+        return $this->commands;
+    }
+
+    /**
+     * Get the value of firstWord
+     */ 
+    public function getFirstWord()
+    {
+        return $this->firstWord;
+    }
+
+    /**
+     * Get the value of otherWords
+     */ 
+    public function getOtherWords()
+    {
+        return $this->otherWords;
+    }
+}
+
+?>
