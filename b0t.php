24b4dce093dbb14c227a55413bfa3de39f55a462
R. Kürşat Vuruşan <kursat.vurusan@ceng.metu.edu.tr>
2018-08-30 14:39:03 +0300

    Update b0t.php

--- b0t.php
+++ b0t.php
@@ -165,7 +165,7 @@ function egonomiadFunc() {
           preg_match_all("/<span data-type=\"son_fiyat\" class=\"LastPrice\" data-secid=\"EURUSD Curncy\">(.*?)<\/span>/msi", $response, $resultRegex);
           $message = $message."\n"."\xF0\x9F\x8F\xA7 avro/dolar paritesi şu an *".$resultRegex[1][0]."*. \xF0\x9F\x8F\xA7";
           preg_match_all("/<span data-type=\"son_fiyat\" class=\"LastPrice\" data-secid=\"IECM2Y Index\">(.*?)<\/span>/msi", $response, $resultRegex);
-          $message = $message."\n"."faiz şu an *".$resultRegex[1][0]."*.";
+          $message = $message."\n"."faiz şu an *%".$resultRegex[1][0]."*.";
           preg_match_all("/<span data-type=\"son_fiyat\" class=\"LastPrice\" data-secid=\"XAU Curncy\">(.*?)<\/span>/msi", $response, $resultRegex);
           $message = $message."\n"."altın/ons şu an *".$resultRegex[1][0]."* TL.";
           preg_match_all("/<span data-type=\"son_fiyat\" class=\"LastPrice\" data-secid=\"CO1 Comdty\">(.*?)<\/span>/msi", $response, $resultRegex);
