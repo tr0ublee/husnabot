a078301b3517c3fd07d9e34cf90b4acff64087eb
chaorrupted <37760447+chaorrupted@users.noreply.github.com>
2018-10-10 16:22:18 +0300

    Update b0t.php

--- b0t.php
+++ b0t.php
@@ -22,6 +22,7 @@ $husnab0t->addCommand("help","helber");
 $husnab0t->addCommand("neyesem","yemeksepeti");
 $husnab0t->addCommand("gunaydin","gunadyinFunc");
 $husnab0t->addCommand("günaydın","gunadyinFunc");
+$husnab0t->addCommand("boşyapıyolar","bojyabmaFunc");
 //$husnab0t->addCommand("komutad","komutad");
 
 /* PUT NEW COMMANDS BELOW */
