6bee7fb12ce0fd804aadb1b050c9427405b26f90
R. Kürşat Vuruşan <kursat.vurusan@ceng.metu.edu.tr>
2018-12-27 01:52:42 +0300

    Update b0t.php

--- b0t.php
+++ b0t.php
@@ -10,20 +10,20 @@ require_once("priv8.php");
 
 $husnab0t->addCommand("husnacim","husnacimFunc");
 $husnab0t->addCommand("hüsnacım","husnacimFunc");
-$husnab0t->addCommand("bilgiad","bilgiadFunc");
-$husnab0t->addCommand("mizahyab","mizahyabFunc");
-$husnab0t->addCommand("fotoad","fotoadFunc");
-$husnab0t->addCommand("yemekad","yemekteNeVar");
-$husnab0t->addCommand("dolarad","dolaradFunc");
-$husnab0t->addCommand("avroad","avroadFunc");
-$husnab0t->addCommand("euroad","avroadFunc");
-$husnab0t->addCommand("egonomiad","egonomiadFunc");
-$husnab0t->addCommand("havadurumuad","havadurumuadFunc");
-$husnab0t->addCommand("helb","helber");
+$husnab0t->addCommand("bilgiad","bilgiadFunc",1);
+$husnab0t->addCommand("mizahyab","mizahyabFunc",1);
+$husnab0t->addCommand("fotoad","fotoadFunc",1);
+$husnab0t->addCommand("yemekad","yemekteNeVar",1);
+$husnab0t->addCommand("dolarad","dolaradFunc",1);
+$husnab0t->addCommand("avroad","avroadFunc",1);
+$husnab0t->addCommand("euroad","avroadFunc",1);
+$husnab0t->addCommand("egonomiad","egonomiadFunc",1);
+$husnab0t->addCommand("havadurumuad","havadurumuadFunc",1);
+$husnab0t->addCommand("helb","helber",1);
 $husnab0t->addCommand("help","helber");
 $husnab0t->addCommand("neyesem","yemeksepeti");
 $husnab0t->addCommand("gunaydin","gunadyinFunc");
-$husnab0t->addCommand("günaydın","gunadyinFunc");
+$husnab0t->addCommand("günaydın","gunadyinFunc",1);
 $husnab0t->addCommand("boşyapıyolar","bojyabmaFunc");
 $husnab0t->addCommand("bojyapıyolar","bojyabmaFunc");
 $husnab0t->addCommand("bosyapiyolar","bojyabmaFunc");
@@ -34,7 +34,7 @@ $husnab0t->addCommand("nasil","beyle");
 $husnab0t->addCommand("java","jaava");
 $husnab0t->addCommand("bilimsiz","bilimsiz");
 $husnab0t->addCommand("muazzam","muazzam");
-//$husnab0t->addCommand("komutad","komutad");
+$husnab0t->addCommand("komutad","komutad");
 
 /* PUT NEW COMMANDS BELOW */
 
