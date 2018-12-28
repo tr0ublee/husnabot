3390e08f15e7b112dd07b9ab78bce8f6ef444a49
Mustafa Ozan Alpay <frozsgy@gmail.com>
2018-12-28 19:49:34 +0300

    Update b0t.functions.php
    removed duplicate husnacurl

--- b0t.functions.php
+++ b0t.functions.php
@@ -382,14 +382,6 @@ function contains($str, array $arr) {
 }
 
 /*oyunad function STARTS*/
-function husnaCurl($url) {
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($ch);
-  curl_close($ch);
-  return $response;
-}
 function oyunfiyatiAd($url2){
     global $husnab0t;
     $tablex="";
