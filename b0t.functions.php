7dd6a9b8e85604014bc87f5e05a37e57b4d79e1c
R. Kürşat Vuruşan <kursat.vurusan@ceng.metu.edu.tr>
2019-03-04 23:43:41 +0300

    Update b0t.functions.php

--- b0t.functions.php
+++ b0t.functions.php
@@ -5,6 +5,7 @@ function husnaCurl($url) {
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch,CURLOPT_TIMEOUT,1000);
   $response = curl_exec($ch);
   curl_close($ch);
   return $response;
