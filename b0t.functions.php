76acedd05e802f1d0a00e73b1e834c5316867afd
furkan karatas <frkn.krts.metu@gmail.com>
2019-03-24 02:55:59 +0300

    zlotiad function regex fixed for the last time, also flag icons fixed

--- b0t.functions.php
+++ b0t.functions.php
@@ -524,8 +524,8 @@ function iyiGeceler() {
 function zlotyadFunc() {
   global $husnab0t;
   $response = husnaCurl("https://www.bloomberg.com/quote/PLNTRY:CUR");
-  preg_match_all("/<span class=\"priceText.*\">(.*?)<\/span>/msi", $response, $resultRegex);
-  $message = "\xF0\x9F\x82\xB9 \xF0\x9F\x87\xB1 zloty şu an *".$resultRegex[1][0]."* ₺ hojam. \xF0\x9F\x87\xB9 \xF0\x9F\x87\xB7";
+  preg_match_all("/<span class=\"priceText.*?\">(.*?)<\/span>/msi", $response, $resultRegex);
+  $message = "\xF0\x9F\x87\xB5\xF0\x9F\x87\xB1 zloty şu an *".$resultRegex[1][0]."* ₺ hojam. \xF0\x9F\x87\xB9\xF0\x9F\x87\xB7";
   $husnab0t->sendMessage($message);
 }
 
